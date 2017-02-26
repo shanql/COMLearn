06695586e841fa74e2e4b3722538512382597ae8
shanql <634772208@qq.com>
2017-02-26 16:17:20 +0800

    add code for chap4

---
+++ Chap4/RefCount.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <ObjBase.h>
+
+using namespace std;
+
+void trace( const char* msg ) { cout << msg << endl;}
+
+// Interface
+interface IX: public IUnknown
+{
+	virtual void __stdcall Fx() = 0;
+};
+
+interface IY: public IUnknown
+{
+	virtual void __stdcall Fy() = 0;
+};
+
+interface IZ: public IUnknown
+{
+	virtual void __stdcall Fz() = 0;
+};
+
+// Forward references for GUIDs
+extern const IID IID_IX;
+extern const IID IID_IY;
+extern const IID IID_IZ;
+
+
+// Component
+class CA: public IX, public IY
+{
+	// IUnknown implementation
+	virtual HRESULT __stdcall QueryInterface( const IID& iid, void** ppv );
+	virtual ULONG __stdcall AddRef();
+	virtual ULONG __stdcall Release();
+
+	// Interface IX implementation
+	virtual void __stdcall Fx() { cout << "Fx" << endl; }
+
+	// Interface IY implementation 
+	virtual void __stdcall Fy() { cout << "Fy" << endl; }
+
+public:
+	CA():m_cRef(0){}
+	~CA(){ trace("CA: Destroy self"); }
+
+private:
+	long m_cRef;
+};
+
+HRESULT __stdcall CA::QueryInterface( const IID& iid, void** ppv )
+{
+	if ( IID_IUnknown == iid )
+	{
+		trace( "QueryInterface: Return pointer to IUnknown");
+		*ppv = static_cast<IX*>( this );
+	}
+	else if ( IID_IX == iid )
+	{
+		trace("QueryInterface: Return pointer to IX");
+		*ppv = static_cast<IX*>(this);
+	}
+	else if ( IID_IY == iid )
+	{
+		trace( "QueryInterface: Return pointer to IY");
+		*ppv = static_cast<IY*>( this );
+	}
+	else
+	{
+		trace("QueryInterface: Interface not supported");
+		*ppv = NULL;
+		return E_NOINTERFACE;
+	}
+
+	reinterpret_cast<IUnknown*>(*ppv)->AddRef();
+	return S_OK;
+}
+
+ULONG __stdcall CA::AddRef()
+{
+	cout << "CA: AddRef = " << m_cRef + 1 << endl;
+	return InterlockedIncrement( &m_cRef );
+}
+
+ULONG __stdcall CA::Release()
+{
+	cout << "CA: Release = " << m_cRef - 1 << endl;
+	if ( InterlockedDecrement( &m_cRef ) == 0 )
+	{
+		delete this;
+		return 0;
+	}
+	return m_cRef;
+}
+
+// Creation function
+IUnknown* CreateInstance()
+{
+	IUnknown* pI = static_cast<IX*>(new CA);
+	pI->AddRef();
+	return pI;
+}
+
+
+// IIDs
+// {2931BFDC-CC73-402a-82A4-980107E2EDA7}
+static const IID IID_IX = 
+{ 0x2931bfdc, 0xcc73, 0x402a, { 0x82, 0xa4, 0x98, 0x1, 0x7, 0xe2, 0xed, 0xa7 } };
+
+// {47B4F184-BE28-47a9-AA1A-F2BA50B0E6A6}
+static const IID IID_IY = 
+{ 0x47b4f184, 0xbe28, 0x47a9, { 0xaa, 0x1a, 0xf2, 0xba, 0x50, 0xb0, 0xe6, 0xa6 } };
+
+// {30C0B9EB-40B2-4a2d-8D2F-CBA192011113}
+static const IID IID_IZ = 
+{ 0x30c0b9eb, 0x40b2, 0x4a2d, { 0x8d, 0x2f, 0xcb, 0xa1, 0x92, 0x1, 0x11, 0x13 } };
+
+
+// Client
+int main()
+{
+	HRESULT hr;
+	trace("Client:Get an IUnknown pointer");
+	IUnknown* pIUnknown = CreateInstance();
+
+	trace("Client:Get interface IX");
+	IX* pIX = NULL;
+	hr = pIUnknown->QueryInterface( IID_IX, (void**)&pIX );
+	if ( SUCCEEDED( hr ) )
+	{
+		trace("Client:Succeeded getting IX");
+		pIX->Fx();
+		pIX->Release();
+	}
+
+	trace("Client:Get interface IY");
+	IY* pIY = NULL;
+	hr = pIUnknown->QueryInterface( IID_IY, (void**)&pIY );
+	if ( SUCCEEDED( hr ) )
+	{
+		trace("Client:Succeeded getting IY");
+		pIY->Fy();
+		pIY->Release();
+	}
+
+	trace("Client:Ask for an unsupported interface");
+	IZ* pIZ = NULL;
+	hr = pIUnknown->QueryInterface( IID_IZ, (void**)&pIZ );
+	if ( SUCCEEDED( hr ) )
+	{
+		trace("Client:Succeeded getting IZ");
+		pIZ->Fz();
+		pIZ->Release();
+	}
+	else
+	{
+		trace("Client:Could not get interface IZ");
+	}
+
+	trace("Client:Release IUnknown interface");
+	pIUnknown->Release();
+
+	return 0;
+}
+
+
