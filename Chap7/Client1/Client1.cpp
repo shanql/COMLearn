3ec77686ce32d1f6f393a4c150bff634c578e58d
shanql <634772208@qq.com>
2017-03-05 11:14:09 +0800

    增加从注册表中注销组件的代码

--- Chap7/Client1/Client1.cpp
+++ Chap7/Client1/Client1.cpp
@@ -59,6 +59,7 @@ BOOL SetKeyAndValue( PTCHAR pszKey, PTCHAR pszSubKey, PTCHAR pszValue )
 	return TRUE;
 }
 
+
 void CLSIDToStr( const CLSID& clsid, PTCHAR pszClsid, int nCountOfElement )
 {
 	LPOLESTR pClsidStr = NULL;
@@ -71,12 +72,9 @@ void CLSIDToStr( const CLSID& clsid, PTCHAR pszClsid, int nCountOfElement )
 	}
 }
 
-void RegisterServer( HMODULE hModule, PTCHAR pszFriendlyName,
+void RegisterServer( HMODULE hModule, const CLSID& clsid, PTCHAR pszFriendlyName,
 					PTCHAR pszProgId, PTCHAR pszVerIndProgID )
 {
-	TCHAR szClsid[48] = {0};
-	CLSIDToStr( CLSID_Component1, szClsid, _countof(szClsid) );
-
 	TCHAR szDllFileName[256] = {0};
 	if ( 0 == GetModuleFileName( hModule,
 		szDllFileName, _countof(szDllFileName) ) )
@@ -86,6 +84,8 @@ void RegisterServer( HMODULE hModule, PTCHAR pszFriendlyName,
 	}
 	
 	// add CLSID under the HKEY_CLASSES_ROOT
+	TCHAR szClsid[48] = {0};
+	CLSIDToStr( clsid, szClsid, _countof(szClsid) );
 	TCHAR szClsKey[64] = {0};
 	_stprintf_s( szClsKey, _countof(szClsKey), _T("CLSID\\%s"), szClsid );
 	if ( !SetKeyAndValue( szClsKey, NULL, pszFriendlyName ) )
@@ -152,10 +152,78 @@ void RegisterServer( HMODULE hModule, PTCHAR pszFriendlyName,
 	trace("register server successfully");
 }
 
+BOOL RecursiveDeleteKey( HKEY hParenKey, PTCHAR pszSubKey )
+{
+	if ( !pszSubKey )
+	{
+		return FALSE;
+	}
+
+	HKEY hChildKey = NULL;
+	if ( ERROR_SUCCESS != RegOpenKeyEx( hParenKey, pszSubKey, 0, KEY_ALL_ACCESS, &hChildKey ) )
+	{
+		return FALSE;
+	}
+
+
+	TCHAR szChildBuf[256] = {0};
+	DWORD dwSize = _countof(szChildBuf);
+	FILETIME time;
+	while( ERROR_SUCCESS == RegEnumKeyEx( hChildKey, 
+		0, szChildBuf, &dwSize, NULL, NULL, NULL, &time) )//第二个参数dwIndex每次传0是因为删除了键
+	{
+		if ( !RecursiveDeleteKey( hChildKey, szChildBuf ) )
+		{
+			return FALSE;
+		}
+		dwSize = _countof(szChildBuf);
+	}
+
+	RegCloseKey( hChildKey );
+	return (BOOL)(ERROR_SUCCESS == RegDeleteKey( hParenKey, pszSubKey ));
+}
+
+void UnRegisterServer( const CLSID& clsid, PTCHAR pszProgId, PTCHAR pszVerIndProgID)
+{
+	TCHAR szClsid[48] = {0};
+	CLSIDToStr( CLSID_Component1, szClsid, _countof(szClsid) );
+	TCHAR szClsKey[64] = {0};
+	_stprintf_s( szClsKey, _countof(szClsKey), _T("CLSID\\%s"), szClsid );
+
+	// delete CLISID\<CLISID>
+	if ( !RecursiveDeleteKey( HKEY_CLASSES_ROOT, szClsKey ) )
+	{
+		return;
+	}
+
+	// delete ProgId under the HKEY_CLASSES_ROOT
+	if ( !RecursiveDeleteKey( HKEY_CLASSES_ROOT, pszProgId ) )
+	{
+		return;
+	}
+
+	// delete version-independent ProgID under the HKEY_CLASSES_ROOT
+	if ( !RecursiveDeleteKey( HKEY_CLASSES_ROOT, pszVerIndProgID ) )
+	{
+		return;
+	}
+
+	trace("unregister server successfully");
+}
+
 int main()
 {
-	RegisterServer( 
-		GetModuleHandle( NULL ),
-		_T("friend 友好名字 name!！！"), _T("Shanql.ComponentTest1.1"), _T("Shanql.ComponentTest1"));
+// 	RegisterServer( 
+// 		GetModuleHandle( NULL ),
+// 		CLSID_Component1,
+// 		_T("friend 友好名字 name!！！"),
+// 		_T("Shanql.ComponentTest1.1"),
+// 		_T("Shanql.ComponentTest1"));
+
+	UnRegisterServer(
+		CLSID_Component1,
+		_T("Shanql.ComponentTest1.1"),
+		_T("Shanql.ComponentTest1") );
+
 	return 0;
 }
